a2e439a8f344b66e07e87e8545704615c32c8d47
Joachim Marchal <jmarchal01@clipper.ens.fr>
2026-03-25 15:30:09 +0100

    casse mais pas trop

--- libc/stdio/printf.c
+++ libc/stdio/printf.c
@@ -42,7 +42,7 @@ int printf(const char* restrict format, ...) {
 
 		switch (*format) {
 
-			case 'c':
+			case 'c': {
 				format++;
 				char c = (char) va_arg(parameters, int /* char promotes to int */);
 				if (!maxrem) {
@@ -52,8 +52,9 @@ int printf(const char* restrict format, ...) {
 				if (!print(&c, sizeof(c)))
 					return -1;
 				written++;
+			}
 
-			case 's':
+			case 's': {
 				format++;
 				const char* str = va_arg(parameters, const char*);
 				size_t len = strlen(str);
@@ -64,8 +65,9 @@ int printf(const char* restrict format, ...) {
 				if (!print(str, len))
 					return -1;
 				written += len;
+			}
 
-			default:
+			default : {
 				format = format_begun_at;
 				size_t len = strlen(format);
 				if (maxrem < len) {
@@ -76,43 +78,8 @@ int printf(const char* restrict format, ...) {
 					return -1;
 				written += len;
 				format += len;
-
+			}
 		}
-
-		/*
-		if (*format == 'c') {
-			format++;
-			char c = (char) va_arg(parameters, int);
-			if (!maxrem) {
-				// TODO: Set errno to EOVERFLOW.
-				return -1;
-			}
-			if (!print(&c, sizeof(c)))
-				return -1;
-			written++;
-		} else if (*format == 's') {
-			format++;
-			const char* str = va_arg(parameters, const char*);
-			size_t len = strlen(str);
-			if (maxrem < len) {
-				// TODO: Set errno to EOVERFLOW.
-				return -1;
-			}
-			if (!print(str, len))
-				return -1;
-			written += len;
-		} else {
-			format = format_begun_at;
-			size_t len = strlen(format);
-			if (maxrem < len) {
-				// TODO: Set errno to EOVERFLOW.
-				return -1;
-			}
-			if (!print(format, len))
-				return -1;
-			written += len;
-			format += len;
-		}*/
 	}
 
 	va_end(parameters);
