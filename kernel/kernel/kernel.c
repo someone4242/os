fea7a1faa670dc57cc692e9fc7c076ade0d8aaa1
Max Holemans <holemans.max@gmail.com>
2026-04-01 14:14:34 +0200

    Enable paging

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -3,10 +3,40 @@
 #include <stdint.h>
 #include <kernel/tty.h>
 
+#define PAGE_BYTE_SIZE 4096
+#define PAGE_SIZE 1024
+
 extern uint32_t _kernel_start;
 extern uint32_t _kernel_end;
 
+extern void loadPageDirectory(uint32_t *);
+extern void enablePaging();
+
+void init_identity_mapping(uint32_t page_directory[], size_t identity_pages) {
+    uint32_t first_page_table[PAGE_SIZE] __attribute__((aligned(4096)));
+
+    for (size_t i = 0; i < PAGE_SIZE; i++) {
+        *(page_directory + i) = 0x0002;
+    }
+    for (size_t i = 0; i < identity_pages; i++) {
+        *(first_page_table + i) = (i * 0x1000) | 3;
+    }
+
+    *(page_directory + 0) = ((uint32_t)first_page_table) | 3;
+}
+
 void kernel_main(multiboot_info_t* mbd, uint32_t magic) {
+    uint32_t start_addr = (uint32_t)&_kernel_start;
+    uint32_t end_addr = (uint32_t)&_kernel_end;
+
+    uint32_t page_directory[PAGE_SIZE] __attribute__((aligned(4096)));
+    size_t identity_pages = end_addr >> 12;
+
+    init_identity_mapping(page_directory, identity_pages);
+    loadPageDirectory(page_directory);
+    enablePaging();
+
+
 	terminal_initialize();
 	printf("Hello, world\n");
     /* Make sure the magic number matches for memory mapping*/
@@ -24,12 +54,9 @@ void kernel_main(multiboot_info_t* mbd, uint32_t magic) {
     }
 
     /* Loop through the memory map and display the values */
-    int i;
-    uint32_t start_addr = (uint32_t)&_kernel_start;
-    uint32_t end_addr = (uint32_t)&_kernel_end;
 
     printf("Le noyau occupe la RAM de %d à %d\n", start_addr, end_addr);
-    for(i = 0; i < mbd->mmap_length; i += sizeof(multiboot_memory_map_t)) {
+    for(size_t i = 0; i < mbd->mmap_length; i += sizeof(multiboot_memory_map_t)) {
         multiboot_memory_map_t* mmmt = (multiboot_memory_map_t*) (mbd->mmap_addr + i);
 
         printf("Start Addr: %d %d | Length: %d %d | Size: %d | Type: %d\n",
